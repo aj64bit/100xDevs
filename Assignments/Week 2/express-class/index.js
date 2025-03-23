be782d9745870c61810b339689811459cb4b9054
Ajit Gavade <ajitgavade73@gmail.com>
2025-03-24 00:49:06 +0530

    express-class added

---
+++ Assignments/Week 2/express-class/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    // res.send('hello');
+    const n = Number(req.query.n);
+    const m = Number(req.query.m);
+    const ms = n + m;
+    res.send("Helloww " + ms.toString());
+});
+
+app.listen(3005)
